d14d46b83b0d64f6b67b3eac7499324d3b977fac
Sebastian Koczotowski <sebastian.koczotowski@gmail.com>
2025-03-10 17:37:44 +0100

    Firebase - envs.

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,16 +1,17 @@
 import {initializeApp} from 'firebase/app'
 import {getFirestore} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
+import {loadEnv} from "vite";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBsUJcmUqfYbEjyw3Rr1HKHfqxc3TCy1Uw",
-    authDomain: "gooner-news.firebaseapp.com",
-    projectId: "gooner-news",
-    storageBucket: "gooner-news.firebasestorage.app",
-    messagingSenderId: "606048921369",
-    appId: "1:606048921369:web:7a1f612b32be1b208e4788",
-    measurementId: "G-JFERTQTHBX"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 initializeApp(firebaseConfig);
